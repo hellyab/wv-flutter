f06e5d375e846e3730fdf80bb8704856a5981886
hellyab <yabsra23@gmail.com>
2020-07-30 17:09:55 +0300

    url update as page changes, added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:string_validator/string_validator.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter WebView',
       theme: ThemeData(
-        primarySwatch: Colors.amber,
+        primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Webview'),
     );
@@ -28,51 +29,101 @@ class _MyHomePageState extends State<MyHomePage> {
   String url = "https://google.com";
   final _controller = TextEditingController();
   WebViewController _webviewController;
+  bool loading = true;
+
+  load() async {
+    if (isURL(_controller.text)) {
+      if (!(_controller.text.startsWith("http://") ||
+          _controller.text.startsWith("https://"))) {
+        _controller.text = 'https://${_controller.text}';
+      }
+      if (_controller.text != await _webviewController.currentUrl()) {
+        _webviewController.loadUrl(_controller.text);
+        setState(() {
+          loading = true;
+        });
+      }
+    } else {
+      Scaffold.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            "Please enter a valid URL.",
+          ),
+        ),
+      );
+    }
+    print(_controller.text);
+  }
 
   @override
   Widget build(BuildContext context) {
     final double width = MediaQuery.of(context).size.width;
-    final double height = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      body: SizedBox(
-        height: height,
-        child: ListView(
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                SizedBox(
-                  width: width * 0.7,
-                  height: height * 0.1,
-                  child: TextField(
-                    autofocus: true,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                    ),
-                    controller: _controller,
-                  ),
-                ),
-                FlatButton(
-                  shape: CircleBorder(),
-                  child: Icon(Icons.navigate_next),
-                  onPressed: () {
-                    print(_controller.text);
-                    setState(() {});
-                    _webviewController.loadUrl(_controller.text);
-                  },
-                )
-              ],
+      appBar: AppBar(
+        backgroundColor: Colors.black87,
+        title: SizedBox(
+          width: width * 0.7,
+          child: TextField(
+            decoration: InputDecoration(
+              border: InputBorder.none,
             ),
-            SizedBox(
-              height: height * 0.9,
-              child: WebView(
-                initialUrl: url,
-                // 'https://anz1-usdc2.etadirect.com/ssmportal/mobility/',
-                javascriptMode: JavascriptMode.unrestricted,
-                onWebViewCreated: (WebViewController controller) {
-                  _webviewController = controller;
+            controller: _controller,
+            keyboardType: TextInputType.url,
+            style: TextStyle(
+              color: Colors.white54,
+            ),
+            onSubmitted: (value) {
+              load();
+            },
+          ),
+        ),
+        actions: <Widget>[
+          Builder(
+            builder: (context) => ButtonTheme(
+              child: FlatButton(
+                shape: CircleBorder(),
+                child: Icon(
+                  Icons.navigate_next,
+                  color: Colors.white54,
+                ),
+                onPressed: () {
+                  load();
+                  FocusScope.of(context).requestFocus(new FocusNode());
                 },
               ),
+              minWidth: width * 0.2,
+              height: width * 0.2,
+            ),
+          ),
+        ],
+      ),
+      body: Container(
+        child: Stack(
+          children: <Widget>[
+            WebView(
+              initialUrl: url,
+              // 'https://anz1-usdc2.etadirect.com/ssmportal/mobility/',
+              javascriptMode: JavascriptMode.unrestricted,
+              onWebViewCreated: (WebViewController controller) {
+                _webviewController = controller;
+              },
+              onPageFinished: (url) async {
+                _controller.text = await _webviewController.currentUrl();
+                setState(() {
+                  loading = false;
+                });
+              },
+              onWebResourceError: (error) {
+                setState(() {
+                  loading = false;
+                });
+              },
+            ),
+            Builder(
+              builder: (context) => loading
+                  ? SizedBox(height: 2.5, child: LinearProgressIndicator())
+                  : SizedBox(),
             ),
           ],
         ),
